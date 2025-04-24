b20b98f95065adc23267c6c391f66f9f9f47f68c
devNeba <148289328+dev231626@users.noreply.github.com>
2025-04-24 06:12:11 -0700

    Update tesla.js

--- tesla.js
+++ tesla.js
@@ -1,10 +1,68 @@
-
 // Tesla.js
 
-var head = document.getElementsByClassName('head')[0]
+// get the dom elements
+var main= document.querySelector(".main-1");
+const menu = document.querySelector("#menu")
+const cross = document.querySelector(".fa-xmark")
+const blurs = document.querySelector(".blur")
+const side = document.querySelector(".hdn-side")
 
-console.log('Width:', head.offsetWidth);
-console.log('Height:', head.offsetHeight);
+// event
+
+let menuVisible = true
+
+menu.addEventListener("click", function(e){
+    e.preventDefault()
+    e.stopPropagation()
+    blurs.classList.add("active")
+    side.classList.add("active")
+
+})
+ 
+cross.addEventListener("click", () =>{
+    blurs.classList.remove("active")
+    side.classList.remove("active")
+})
+
+// Fix the cross button event listener
+const mark = document.querySelector("#mark .fa-xmark");
+mark.addEventListener("click", () => {
+    blurs.classList.remove("active");
+    side.classList.remove("active");
+});
+
+const h1 = document.querySelector(".head-1")
+const h2 = document.querySelector(".head-2")
+const h3 = document.querySelector(".head-3")
+const h4 = document.querySelector(".head-4")
+const h5 = document.querySelector(".head-5")
+const h6 = document.querySelector(".head-6")
+
+const l1 = document.querySelector(".login-1")
+const l2 = document.querySelector(".login-2")
+const l3 = document.querySelector(".login-3")
+const l4 = document.querySelector(".login-4")
+const l5 = document.querySelector(".login-5")
+const l6 = document.querySelector(".login-6");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
